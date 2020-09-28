60770800b81079093543f98f18db72f890f02adb
joliw9282 <sizhen.wu@mail.utoronto.ca>
2020-09-28 03:20:21 +0000

    second commit

--- assignment 1.R
+++ assignment 1.R
@@ -17,17 +17,37 @@ blogdown::new_site(
   serve = interactive()
 )
 
-blogdown::new_post(
-  "page 2",
-  kind = "",
-  open = interactive(),
-  author = getOption("blogdown.author"),
-  categories = NULL,
-  tags = NULL,
-  date = Sys.Date(),
-  file = NULL,
-  slug = NULL,
-  title_case = getOption("blogdown.title_case"),
-  subdir = getOption("blogdown.subdir", "post"),
-  ext = getOption("blogdown.ext", ".rmd")
-)
+
+install.packages("opendatatoronto")
+install.packages("devtools")
+
+library(opendatatoronto)
+library(devtools)
+
+# get package
+package <- show_package("64b54586-6180-4485-83eb-81e8fae3b8fe")
+package
+
+# get all resources for this package
+resources <- list_package_resources("64b54586-6180-4485-83eb-81e8fae3b8fe")
+resources
+
+# identify datastore resources; by default, Toronto Open Data sets datastore resource format to CSV for non-geospatial and GeoJSON for geospatial resources
+datastore_resources <- filter(resources, tolower(format) %in% c('csv', 'geojson'))
+
+# load the first datastore resource as a sample
+data <- filter(datastore_resources, row_number()==1) %>% get_resource()
+data
+
+blogdown::serve_site()
+
+names(data)[names(data) == "Age Group"] <- "Age"
+ggplot(data, aes(x = Age)) + geom_bar(width = 0.5)
+
+names(data)[names(data) == "Age Group"] <- "Age"
+# names(data)[names(data) == "Client Gender"] <- "Gender"
+ggplot(data, aes(x = Age)) + geom_bar(fill = Outcome)
+
+ggplot(data, aes(x = Outcome)) + geom_bar()
+
+
